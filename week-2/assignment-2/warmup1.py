21a8d0289fbdc009856f9979f685cf61c4a01a4a
talita <taliitacamargo@icloud.com>
2026-07-14 13:52:06 -0400

    Add warmup structure folders & designated files

---
+++ week-2/assignment-2/warmup1.py
@@ -0,0 +1,4 @@
+# Command used: python3 warmup1.py
+# Output: Python is working!
+
+print("Python is working!")
---
+++ week-2/assignment-2/warmup2.py
@@ -0,0 +1,6 @@
+# Commands used: mkdir {foldernames}, touch {filename.py}, cd week-2/assignment-2
+
+
+today_date = input("What's today's date? ")
+
+print(f" Today is {today_date}")
